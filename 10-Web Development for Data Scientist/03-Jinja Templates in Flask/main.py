0b18eb6218b0e2db8c0e5ee8faf07d977c49bbe9
naitik2043 <206251886+naitik2043@users.noreply.github.com>
2026-07-15 10:51:59 +0530

    Jinja in Flask

---
+++ 10-Web Development for Data Scientist/03-Jinja Templates in Flask/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    name = "Jill"
+    language = "JavaScript"
+    luckynos = [1, 5, 76, 86, 99, 100]
+    footer = "<p>Copyright 2025 | All rights reserved</p>"
+
+    return render_template(
+        "index.html",
+        name=name,
+        lang=language,
+        lucky=luckynos,
+        footer=footer
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
